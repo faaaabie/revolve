5119b90ccbe3e78fec6e16998a1b18abe824ed16
Fabienne Kick <48417865+faaaabie@users.noreply.github.com>
2020-10-25 21:37:31 +0100

    Update standard_mutation.py

--- pyrevolve/genotype/plasticoding/mutation/standard_mutation.py
+++ pyrevolve/genotype/plasticoding/mutation/standard_mutation.py
@@ -251,8 +251,7 @@ def standard_mutation(genotype, mutation_conf):
                 modified_genotype = handle_swap_plastic(new_genotype)
             elif mutation_type == 3:
                 modified_genotype = handle_addition_plastic(new_genotype, mutation_conf.genotype_conf)
-            elif mutation_type == 4:
-                pass
+            #elif mutation_type == 4:
                 #modified_genotype = handle_clause(new_genotype, mutation_conf)
             else:
                 raise Exception(
