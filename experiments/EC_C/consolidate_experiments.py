916bf568146d64b45c4897143f553baf27d78f33
Fabienne Kick <48417865+faaaabie@users.noreply.github.com>
2020-12-09 12:46:16 +0100

    Update consolidate_experiments.py

--- experiments/EC_C/consolidate_experiments.py
+++ experiments/EC_C/consolidate_experiments.py
@@ -15,7 +15,7 @@ environments = {
   'cost_of_regulation_big': ['plane','tilted5']
 # ,'baseline_big': ['plane','tilted5']
                  }
-runs = range(1,11)
+runs = range(1,21)
 
 
 # set these variables according to your experiments #
