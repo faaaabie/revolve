e9de96e5c128e1ddd240fd314cd6f12e52a93b28
Fabienne Kick <48417865+faaaabie@users.noreply.github.com>
2020-12-16 15:39:36 +0100

    Update best_robots.R

--- experiments/EC_C/best_robots.R
+++ experiments/EC_C/best_robots.R
@@ -70,12 +70,10 @@ for(m in 1:length(paths))
               bodies = c(bodies, body)
              }
            }
+            side_by_side = image_append(bodies, stack=F)
          }
-          
-       side_by_side = image_append(bodies, stack=F)
-        
+        image_write(side_by_side, path = paste(output_directory,"/",paths[m],'_', environments[[m]][env], "_bodies_best_",exp,".png",sep=''), format = "png")
     }
-    image_write(side_by_side, path = paste(output_directory,"/",paths[m],'_', environments[[m]][env], "_bodies_best_",exp,".png",sep=''), format = "png")
 }
 
 
